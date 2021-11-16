c0fa048c3608a5707f4a813d4aa4a6c895de9928
Ankush Dhiman <ankush.dhiman@wildstyle-network.com>
2021-11-17 00:06:29 +0100

    theme setup

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,30 +1,35 @@
-const colors = require('tailwindcss/colors')
-
 module.exports = {
   mode: 'jit',
   purge: ['./src/**/*.tsx'],
   theme: {
+    spacing: {
+      '0.25': '2px',
+      '0.5': '4px',
+      '1': '8px',
+      '1.5': '12px',
+      '2': '16px',
+      '3': '24px',
+      '4': '32px',
+      '5': '40px',
+      '6': '48px',
+      '7': '56px',
+      '8': '64px',
+      '9': '72px',
+      '10': '80px',
+    },
     colors: {
       transparent: 'transparent',
       current: 'currentColor',
-      black: colors.black,
-      white: colors.white,
-      green: colors.green,
-      indigo: colors.indigo,
-      red: colors.red,
-      yellow: colors.amber,
-    },
-    extend: {
-      spacing: {
-        '1/2': '50%',
-        '1/4': '25%',
-        '1/5': '20%',
-        '1/10': '10%',
-        'fit': 'fit-content',
-      },
+      primary: '#1f5a6d',
+      secondary: '#1374f5',
+      brand: '#ff8001',
+      layover: 'rgba(0, 0, 0, 0.2)',
+      white: '#ffffff',
+      body: '#f8f5f2',
     },
   },
   corePlugins: {
     fontFamily: false,
+    container: false,
   }
 };
